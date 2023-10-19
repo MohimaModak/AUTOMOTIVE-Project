12dd9e2bc9a44642adc469b0f3e82bc78140e460
mohima13 <modakmohima46@gmail.com>
2023-10-19 16:30:06 +0600

    firebase configaration changed

--- src/Components/Firebase/firebase.config.js
+++ src/Components/Firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4q2Z3gUnt_lk0M42hPr_hubUFtHxTiV4",
-  authDomain: "assignment-ten-project-85a1e.firebaseapp.com",
-  projectId: "assignment-ten-project-85a1e",
-  storageBucket: "assignment-ten-project-85a1e.appspot.com",
-  messagingSenderId: "748997514002",
-  appId: "1:748997514002:web:eb9260f6d3eb6538c719cb",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
